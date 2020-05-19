6da71136d443bfdc614746541fa225fc4cd68222
Carol Vasconcelos <cvsouza04@gmail.com>
2020-05-18 22:50:30 -0300

    Fix bottom navigation bug

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { createBottomTabNavigation } from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import PayScreen from './screens/Pay';
 import HomeScreen from './screens/Home';
 import WalletScreen from './screens/Wallet';
 
-const Tab = createBottomTabNavigation();
+const Tab = createBottomTabNavigator();
 
 export default function Navigation() {
   return (
-    <Tab.Navigation>
+    <Tab.Navigator>
       <Tab.Screen name="Home" component={HomeScreen}></Tab.Screen>
       <Tab.Screen name="Wallet" component={WalletScreen}></Tab.Screen>
       <Tab.Screen name="Pay" component={PayScreen}></Tab.Screen>
-    </Tab.Navigation>
+    </Tab.Navigator>
   );
 }
